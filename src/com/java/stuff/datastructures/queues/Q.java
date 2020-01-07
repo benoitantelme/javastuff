d0f68e09eadb3ace4fe0f28fbb6b81513fe16679
benoitantelme <benoitantelme@gmail.com>
2020-01-07 16:58:59 +0000

    Methods to reverse queue iter and recurs

--- src/com/java/stuff/datastructures/queues/Q.java
+++ src/com/java/stuff/datastructures/queues/Q.java
@@ -3,6 +3,7 @@ package com.java.stuff.datastructures.queues;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Q {
 
@@ -30,12 +31,56 @@ public class Q {
         return pageFaults;
     }
 
+    static void reverseQueueIt(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+
+        while(!queue.isEmpty())
+            stack.push(queue.poll());
+
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+    }
+
+    static void reverseQueueRec(Queue<Integer> queue){
+        Integer i = queue.poll();
+        if(!queue.isEmpty())
+            reverseQueueRec(queue);
+
+        queue.add(i);
+    }
+
+    static void printQueue(Queue<Integer> queue){
+        System.out.println("Queue contains:");
+        for(Integer i : queue)
+            System.out.println(i);
+    }
+
     public static void main(String[] args) {
         int pages[] = {7, 0, 1, 2, 0, 3, 0, 4,
                 2, 3, 0, 3, 2};
 
         System.out.println("For pages: " + Arrays.toString(pages));
         System.out.println("Number of page faults with 4 pages in memory: " + pageFaults(pages, 4));
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        queue.add(60);
+        queue.add(70);
+        queue.add(80);
+        queue.add(90);
+        queue.add(100);
+
+        printQueue(queue);
+
+        reverseQueueIt(queue);
+        printQueue(queue);
+
+        reverseQueueRec(queue);
+        printQueue(queue);
     }
 
 }
