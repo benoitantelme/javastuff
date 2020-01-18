f88dfd2a0577144f836b61e2dd28898490b609e9
benoitantelme <benoitantelme@gmail.com>
2020-01-18 10:32:33 +0000

    iterative quicksort with stack

--- src/com/java/stuff/sort/Quicksort.java
+++ src/com/java/stuff/sort/Quicksort.java
@@ -1,27 +1,30 @@
 package com.java.stuff.sort;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Quicksort {
 
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
     static private int partition(int[] array, int min, int max){
         int pivot = array[max];
-        int i = min-1;
+        int i = min;
 
         for(int j = min; j < max; j++){
             if(array[j] < pivot){
+                swap(array, i, j);
                 i++;
-                int tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
             }
         }
 
-        int tmp = array[max];
-        array[max] = array[i+1];
-        array[i+1] = tmp;
-
-        return i+1;
+        swap(array, i, max);
+        return i;
     }
 
     static void sort(int[] array, int min, int max){
@@ -33,10 +36,41 @@ public class Quicksort {
         }
     }
 
+    static void iterative(int[] array){
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(0);
+        stack.push(array.length-1);
+
+        while(!stack.isEmpty()){
+            int max = stack.pop();
+            int min = stack.pop();
+
+            int pivot = partition(array, min, max);
+
+
+            if (pivot - 1 > min )
+            {
+                stack.push(min);
+                stack.push(pivot -1);
+            }
+            if (pivot + 1 < max)
+            {
+                stack.push(pivot + 1);
+                stack.push(max);
+            }
+        }
+    }
+
     public static void main(String args[]) {
-        int arr[] = {10, 7, 8, 9, 1, 5};
+        int[] arr = {10, 7, 8, 9, 1, 5};
         System.out.println(Arrays.toString(arr));
         Quicksort.sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+        arr = new int[]{4, 1, 10, 23, 5};
+        System.out.println(Arrays.toString(arr));
+        Quicksort.iterative(arr);
         System.out.println(Arrays.toString(arr));
     }
 
