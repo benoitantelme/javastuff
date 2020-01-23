f5c25d7542cb6246cf25cc5295f6f60e31529dab
benoitantelme <benoitantelme@gmail.com>
2020-01-23 22:20:11 +0000

    Leet code exercises 2

--- src/com/java/stuff/leetcode/ExercisesOne.java
+++ src/com/java/stuff/leetcode/ExercisesOne.java
@@ -50,12 +50,54 @@ public class ExercisesOne {
         return result;
     }
 
+    public static int[] sortedSquares(int[] A) {
+        int[] result = new int[A.length];
+        int i = 0;
+        int j = A.length - 1;
+        int k = A.length - 1;
 
-    public static void main(String args[]) { ;
-        System.out.println(defangIPaddr("1.1.1.1"));
-        System.out.println(Arrays.toString(decompressRLElist(new int[]{1,2,3,4})));
-        System.out.println(Arrays.toString(sumZero(1)));
+        while(i <= j){
+            int isquare = A[i]*A[i];
+            int jsquare = A[j]*A[j];
+            if(isquare > jsquare) {
+                result[k--] = isquare;
+                i++;
+            }
+            else {
+                result[k--] = jsquare;
+                j--;
+            }
+        }
 
+        return result;
+    }
+
+    public static int fib(int N) {
+        int res = 0;
+        int a = 0;
+        int b = 1;
+
+        if(N == 0)
+            return a;
+        if(N == 1)
+            return b;
+
+        for(int i = 1; i < N; i++){
+            res = a + b;
+            a = b;
+            b = res;
+        }
+
+        return res;
+    }
+
+
+    public static void main(String args[]) {
+//        System.out.println(defangIPaddr("1.1.1.1"));
+//        System.out.println(Arrays.toString(decompressRLElist(new int[]{1,2,3,4})));
+//        System.out.println(Arrays.toString(sumZero(1)));
+//        System.out.println(Arrays.toString(sortedSquares(new int[]{-7,-3,2,3,11})));
+        System.out.println(fib(3));
 
     }
 
