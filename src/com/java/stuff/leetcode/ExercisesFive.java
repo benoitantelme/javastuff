2ed0ed67696fe7f257d2c067b9a91076d18fd4e9
benoitantelme <benoitantelme@gmail.com>
2020-01-25 16:04:28 +0000

    More leetcode strings related exercises

--- src/com/java/stuff/leetcode/ExercisesFive.java
+++ src/com/java/stuff/leetcode/ExercisesFive.java
@@ -1,5 +1,9 @@
 package com.java.stuff.leetcode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ExercisesFive {
 
     public static int balancedStringSplit(String s) {
@@ -22,21 +26,76 @@ public class ExercisesFive {
         StringBuilder sb = new StringBuilder();
         char[] array = s.toCharArray();
         for(int i = 0; i < s.length(); i++){
+            int charoffset;
             if(i < s.length()-2 && array[i+2] == '#'){
-                sb.append((char) ((Integer.parseInt("" + array[i] + array[i+1])) + 96));
+                charoffset = Integer.parseInt("" + array[i] + array[i+1]);
                 i +=2;
-            }else {
-                sb.append((char) (Character.getNumericValue(array[i]) + 96));
-            }
+            }else
+                charoffset = Character.getNumericValue(array[i]);
+
+
+            sb.append((char) (charoffset + 96));
         }
 
         return sb.toString();
+    }
+
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+
+        while(i < j){
+            char tmp = s[i];
+            s[i] = s[j];
+            s[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    public static int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+
+        for(String email : emails){
+            String[] splittedEmail = email.split("@");
+
+            String localName = splittedEmail[0];
+            if(localName.contains("+"))
+                localName = localName.substring(0, localName.indexOf("+"));
+
+            set.add(localName.replace(".", "") + "@" + splittedEmail[1]);
+        }
+
+        return set.size();
+    }
+
+    public static int findLUSlength(String a, String b) {
+        int lengtha = a.length();
+        if(a.equals(b))
+            return -1;
+
+        int lengthb = b.length();
+        if(lengtha == lengthb)
+            return lengtha;
+        else if(lengtha > lengthb)
+            return lengtha;
+        else
+            return lengthb;
     }
 
     public static void main(String args[]) {
         System.out.println(balancedStringSplit("RLLLLRRRLR"));
         System.out.println(freqAlphabets("1326#"));
 
+        char[] a = new char[]{'h','e','l','l','o'};
+        System.out.println(Arrays.toString(a));
+        reverseString(a);
+        System.out.println(Arrays.toString(a));
+
+        System.out.println(numUniqueEmails(new String[]{"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"}));
+
+        System.out.println(findLUSlength("aaa", "aaa"));
     }
 
 }
