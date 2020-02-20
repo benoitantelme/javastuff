d66e5bc501714901dd0b4cda43e96ec3caaaa52d
benoitantelme <benoitantelme@gmail.com>
2020-02-20 18:58:07 +0000

    leetcode medium tree exercise and some fibo's

--- src/com/java/stuff/leetcode/ExercisesFourteen.java
+++ src/com/java/stuff/leetcode/ExercisesFourteen.java
@@ -1,4 +1,43 @@
 package com.java.stuff.leetcode;
 
 public class ExercisesFourteen {
+
+    static int fiboIt(int n) {
+        int a = 0;
+        int b = 1;
+
+        if (n == 0) return a;
+        if (n == 1) return b;
+
+        for (int i = 2; i < n+1; i++) {
+            int tmp = b;
+            b = a + b;
+            a = tmp;
+        }
+
+
+        return b;
+    }
+
+    private static int fiboRecu(int n, int a, int b, int count) {
+        if (count >= n+1)
+            return b;
+        else
+            return fiboRecu(n, b, a + b, count + 1);
+    }
+
+    static int fiboRec(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fiboRecu(n, 0, 1, 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fiboIt(4));
+        System.out.println(fiboRec(4));
+
+
+    }
+
 }
--- src/com/java/stuff/leetcode/medium/TreeMedium.java
+++ src/com/java/stuff/leetcode/medium/TreeMedium.java
@@ -1,4 +1,37 @@
 package com.java.stuff.leetcode.medium;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class TreeMedium {
+
+    static public List<Integer> pathInZigZagTree(int label) {
+        LinkedList<Integer> res = new LinkedList<>();
+
+        int depht = (int) (Math.log(label)/ Math.log(2));
+        int move;
+
+        while (depht > 0){
+            int pow = (int) Math.pow(2, depht);
+            move = label - pow;
+            res.addFirst(label);
+            int a = (int) Math.ceil(move / 2.0);
+            label = pow - (int) Math.ceil(move /2) - 1;
+            depht -= 1;
+        }
+
+        res.addFirst(1);
+
+        return res;
+    }
+
+
+    public static void main(String args[]){
+        System.out.println(pathInZigZagTree(16));
+        System.out.println(pathInZigZagTree(14));
+        System.out.println(pathInZigZagTree(26));
+    }
+
+
+
 }
