d9ea1575e2904390eba31bc513c2b587bd8beae1
benoitantelme <benoitantelme@gmail.com>
2020-02-21 17:37:16 +0000

    leetcode medium string exercise

--- src/com/java/stuff/leetcode/medium/StringMedium.java
+++ src/com/java/stuff/leetcode/medium/StringMedium.java
@@ -55,14 +55,40 @@ public class StringMedium {
         return result;
     }
 
-//    public static List<List<String>> groupAnagrams(String[] strs) {
-//
-//
-//        List<List<String>> result = new ArrayList<>();
-//
-//
-//        return result;
-//    }
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+
+        Map<Character, Integer> occurences = new HashMap<>();
+        for(char c : p.toCharArray())
+            occurences.put(c, occurences.getOrDefault(c, 0)+1);
+
+        Map<Character, Integer> tmp = new HashMap<>();
+        for(int i = 0; i < s.length()-p.length()+1; i++) {
+            // first pass
+            if(i == 0){
+                for(int j = i; j < i+p.length(); j++){
+                    char c = s.charAt(j);
+                    tmp.put(c, tmp.getOrDefault(c, 0)+1);
+                }
+            }else{
+                // manage first char of last string
+                char prev = s.charAt(i - 1);
+                int times = tmp.get(prev);
+                if(times == 1)
+                    tmp.remove(prev);
+                else
+                    tmp.put(prev, times -1);
+
+                char c = s.charAt(i+ p.length()-1);
+                tmp.put(c, tmp.getOrDefault(c, 0)+1);
+            }
+
+            if(tmp.equals(occurences))
+                res.add(i);
+        }
+
+        return res;
+    }
 
 
     public static void main(String[] args) {
@@ -70,9 +96,12 @@ public class StringMedium {
         System.out.println(Arrays.toString(maxDepthAfterSplit("()(())()")));
         System.out.println(Arrays.toString(maxDepthAfterSplit("(((()))((())))")));
 
-//        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
-//        System.out.println(groupAnagrams(new String[]{"","b",""}));
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(groupAnagrams(new String[]{"","b",""}));
         System.out.println(groupAnagrams(new String[]{"tea","and","ate","eat","den"}));
+
+        System.out.println(findAnagrams("cbaebabacd", "abc"));
+        System.out.println(findAnagrams("abab", "ab"));
 
     }
 
