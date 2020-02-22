34a7a808fc5ac7b6160bf70c553d92f477c2f526
benoitantelme <benoitantelme@gmail.com>
2020-02-22 13:51:12 +0000

    another leetcode medium graph exercise

--- src/com/java/stuff/leetcode/medium/GraphMedium.java
+++ src/com/java/stuff/leetcode/medium/GraphMedium.java
@@ -30,10 +30,37 @@ public class GraphMedium {
         return set.isEmpty();
     }
 
+    public static boolean canReach(int[] arr, int start) {
+        boolean[] visited = new boolean[arr.length];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while(!queue.isEmpty()){
+            Integer current = queue.poll();
+
+            if(visited[current])
+                continue;
+
+            visited[current] = true;
+            int move = arr[current];
+            if(move == 0)
+                return true;
+
+            if(current + move <= arr.length-1)
+                queue.add(current+move);
+            if(current - move >= 0)
+                queue.add(current-move);
+        }
+
+        return false;
+    }
+
     public static void main(String[] args){
         System.out.println(canVisitAllRooms(List.of(List.of(1), List.of(2), List.of(3), new ArrayList<>())));
 
-        
+        System.out.println(canReach(new int[]{4,2,3,0,3,1,2}, 5));
+
     }
 
 
