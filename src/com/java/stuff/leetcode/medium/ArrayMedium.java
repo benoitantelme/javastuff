cfeaab885f0842d7f39ed84aa39c121582c9c962
benoitantelme <benoitantelme@gmail.com>
2020-02-24 20:06:27 +0000

    leetcode medium array count square subarrays

--- src/com/java/stuff/leetcode/medium/ArrayMedium.java
+++ src/com/java/stuff/leetcode/medium/ArrayMedium.java
@@ -1,4 +1,36 @@
 package com.java.stuff.leetcode.medium;
 
 public class ArrayMedium {
+
+    public int countSquares(int[][] matrix) {
+        int res = 0;
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (i != 0 && j != 0 && matrix[j][i] == 1) {
+                    // 2 for the first bloc + 3 in a 4 blocs square
+                    // 3 for the first, 6 for the 3 blocs next + 5 for a 9 blocs square
+                    // rewriting the value to be reused for mins
+                    int min = Math.min(
+                            Math.min(matrix[j - 1][i], matrix[j - 1][i - 1]),
+                            matrix[j][i - 1]) +1;
+                    matrix[j][i] = min;
+                }
+
+                res += matrix[j][i];
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ArrayMedium am = new ArrayMedium();
+        System.out.println(am.countSquares(new int[][]{
+                {0, 1, 1, 1},
+                {1, 1, 1, 1},
+                {0, 1, 1, 1}
+        }));
+    }
+
 }
