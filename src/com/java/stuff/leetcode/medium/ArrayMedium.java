0f9a1bdf05a57423a2dd7c5ad1fb0483ea58ec06
benoitantelme <benoitantelme@gmail.com>
2020-02-27 21:05:32 +0000

    leetcode medium array exercise

--- src/com/java/stuff/leetcode/medium/ArrayMedium.java
+++ src/com/java/stuff/leetcode/medium/ArrayMedium.java
@@ -110,6 +110,25 @@ public class ArrayMedium {
         return result;
     }
 
+    public int[] productExceptSelf(int[] nums) {
+        final int length = nums.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+
+        left[0] = 1;
+        right[length-1] = 1;
+        for(int i = 1; i < length; i++){
+            left[i] = left[i-1] * nums[i-1];
+            right[length-i-1] = right[length-i] * nums[length-i];
+        }
+
+        int[] result = new int[length];
+        for(int i = 0; i < length; i++)
+            result[i] = left[i]*right[i];
+
+        return result;
+    }
+
     public static void main(String[] args) {
         ArrayMedium am = new ArrayMedium();
         System.out.println(am.countSquares(new int[][]{
@@ -131,6 +150,7 @@ public class ArrayMedium {
         System.out.println(am.queensAttacktheKing(new int[][]
         {{0,0},{1,1},{2,2},{3,4},{3,5},{4,4},{4,5}, {4, 2}}, new int[]{3,3}));
 
+        System.out.println(Arrays.toString(am.productExceptSelf(new int[]{1,2,3,4})));
     }
 
 }
