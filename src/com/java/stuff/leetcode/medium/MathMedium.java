adc0582425270007c34d1abbc6e2031bad30dcd4
benoitantelme <benoitantelme@gmail.com>
2020-02-28 18:23:20 +0000

    leetcode medium math problem

--- src/com/java/stuff/leetcode/medium/MathMedium.java
+++ src/com/java/stuff/leetcode/medium/MathMedium.java
@@ -27,6 +27,24 @@ public class MathMedium {
             return myPow(x*x, n/2) * x;
     }
 
+    private String[] getCoord(String s){
+        String[] coord = s.split("\\+");
+        coord[1] = coord[1].substring(0, coord[1].length()-1);
+        return coord;
+    }
+    public String complexNumberMultiply(String a, String b) {
+        String[] coorda = getCoord(a);
+        String[] coordb = getCoord(b);
+
+        //  (1 + i) * (1 + i) = 1 + i^2 + 2 * i = 2i
+        int im = Integer.valueOf(coorda[0]) * Integer.valueOf(coordb[1]) + Integer.valueOf(coorda[1]) * Integer.valueOf(coordb[0]);
+        int re = Integer.valueOf(coorda[0]) * Integer.valueOf(coordb[0]) - Integer.valueOf(coorda[1]) * Integer.valueOf(coordb[1]);
+
+        String res = re + "+" + im + "i";
+
+        return res;
+    }
+
 
     public static void main(String[] args){
         MathMedium mm = new MathMedium();
@@ -35,7 +53,8 @@ public class MathMedium {
         System.out.println(mm.myPow(2.00000, -2));
         System.out.println(mm.myPow(0.00001, 2147483647));
         System.out.println(mm.myPow(2.00000,-2147483648));
-
+        System.out.println(mm.complexNumberMultiply("1+1i", "1+1i"));
+        System.out.println(mm.complexNumberMultiply("1+-1i", "1+-1i"));
     }
 
 }
