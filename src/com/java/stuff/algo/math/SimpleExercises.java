b3506e07b25c26c7cf4ca16dfed2dad152e52d08
benoitantelme <benoitantelme@gmail.com>
2020-03-01 12:04:55 +0000

    Simple maths getting a median element or value with heaps to manage overflow

--- src/com/java/stuff/algo/math/SimpleExercises.java
+++ src/com/java/stuff/algo/math/SimpleExercises.java
@@ -1,8 +1,6 @@
 package com.java.stuff.algo.math;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class SimpleExercises {
 
@@ -38,17 +36,65 @@ public class SimpleExercises {
         return result;
     }
 
-    public void median(){
+    public void medianValue(){
         Random rdm = new Random();
 
         List<Integer> inputs = new ArrayList<>();
         List<Integer> medians = new ArrayList<>();
         int total = 0;
         for(int i = 0; i < 500; i++) {
-            int next = rdm.nextInt(100);
+            int next = rdm.nextInt(1000000000);
             inputs.add(next);
             total += next;
             medians.add(total/inputs.size());
+        }
+
+        System.out.println("Inputs:");
+        System.out.println(inputs);
+        System.out.println("Medians:");
+        System.out.println(medians);
+    }
+
+    private double getMedian(PriorityQueue<Integer> minh, PriorityQueue<Integer> maxh){
+        if(minh.size() > maxh.size())
+            return minh.peek();
+        else if(minh.size() < maxh.size())
+            return maxh.peek();
+        else  if(minh.isEmpty() || maxh.isEmpty())
+            return 0;
+        return (minh.peek()+maxh.peek())/2;
+    }
+    public void medianElement(){
+        Random rdm = new Random();
+
+        List<Integer> inputs = new ArrayList<>();
+        PriorityQueue<Integer> minh = new PriorityQueue<>();
+        PriorityQueue<Integer> maxh = new PriorityQueue<>(Comparator.reverseOrder());
+        List<Double> medians = new ArrayList<>();
+
+        for(int i = 0; i < 500; i++) {
+            int next = rdm.nextInt(1000000000);
+            inputs.add(next);
+
+            double median = getMedian(minh, maxh);
+            if(next > median){
+                minh.add(next);
+            }else {
+                minh.add(next);
+            }
+
+            int minsize = minh.size();
+            int maxsize = maxh.size();
+            if(Math.abs(minsize - maxsize) > 1){
+                if(minsize > maxsize){
+                    maxh.add(minh.poll());
+                }else{
+                    minh.add(maxh.poll());
+                }
+            }
+
+            median = getMedian(minh, maxh);
+            medians.add(median);
         }
 
         System.out.println("Inputs:");
@@ -65,8 +111,11 @@ public class SimpleExercises {
         System.out.println(sp.randomOfInput(List.of(5, 8, 159, 753, 26, 48, 59, 14785, 6, 4, 2, 8, 9, 7, 1, 3, 598,
                 8468, 24984, 1351, 897, 846, 6256, 84825, 636236, 84484, 2222,9595)));
 
+        System.out.println("Median Value");
+        sp.medianValue();
 
-        sp.median();
+        System.out.println("Median Element");
+        sp.medianElement();
     }
 
 
